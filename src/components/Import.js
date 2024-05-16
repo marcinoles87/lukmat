48f4e46734b57fc7007fb9582ecd4fbe57033a6e
marcinoles87 <marcinoles87@gmail.com>
2024-05-16 16:13:37 +0200

    add pages and components

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,20 +1,34 @@
 import React from 'react'
 
+import import1 from '../img/import1.avif'
+import import2 from '../img/import2.jpg'
+
 function Import
 () {
   return (
     <div className='import-container'>
         <div className='import-header'>
             <h1>NAJLEPSZA OFERTA DOTYCZACA SPROWADZANIA AUT</h1>
-            <p>Odkryj wszystkie zalety PEUGEOT bez wychodzenia z domu i zamów wymarzony model w dowolnej chwili.</p>
+            <p>Odkryj wszystkie zalety współpracy bez wychodzenia z domu i zamów wymarzony model w dowolnej chwili.</p>
         </div>
 
         <div className='import-cards'>
             <div className='import-card'>
-                <a></a>
-                <h2>TEST</h2>
-                <p>Info</p>
+                <img src={import1} alt='import'></img>
+                <h2>Bez względu na wielkośc</h2>
+                <p>Odkryj wszystkie zalety współpracy bez wychodzenia z domu i zamów wymarzony model w dowolnej chwili.</p>
             </div>
+
+            <div className='import-card'>
+            <img src={import2} alt='import'></img>
+                <h2>Współpraca</h2>
+                <p>Odkryj wszystkie zalety współpracy bez wychodzenia z domu i zamów wymarzony model w dowolnej chwili.</p>
+            </div>
+
+           
+
+            
+            
         </div>
 
     </div>
