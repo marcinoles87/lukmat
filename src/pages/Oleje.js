83b4befc73468a00f27ed693b1057b456779b415
marcinoles87 <marcinoles87@gmail.com>
2024-06-17 15:36:46 +0200

    add section

--- src/pages/Oleje.js
+++ src/pages/Oleje.js
@@ -1,8 +1,71 @@
 import React from 'react'
 
+import castrol from '../img/audiLogo.png'
+
 function Oleje() {
+
+  const olejeProdukt = [
+    {
+      img :castrol ,
+      nazwa :'Castrol GTX' ,
+      pojemnosc : '1 L' ,
+      cena : '99,99'
+    } ,
+
+    {
+      img :castrol ,
+      nazwa :'Castrol GTX' ,
+      pojemnosc : '1 L' ,
+      cena : '99,99'
+    } ,
+
+    {
+      img :castrol ,
+      nazwa :'Castrol GTX' ,
+      pojemnosc : '1 L' ,
+      cena : '99,99'
+    } , 
+
+    {
+      img :castrol ,
+      nazwa :'Castrol GTX' ,
+      pojemnosc : '1 L' ,
+      cena : '99,99'
+    } ,
+
+    {
+      img :castrol ,
+      nazwa :'Castrol GTX' ,
+      pojemnosc : '1 L' ,
+      cena : '99,99'
+    } ,
+
+    {
+      img :castrol ,
+      nazwa :'Castrol GTX' ,
+      pojemnosc : '1 L' ,
+      cena : '99,99'
+    } ,
+  ]
   return (
-    <div>Oleje</div>
+    <div className='oleje-container'>
+      <div className='oleje-head'>
+        <h1>Oleje i inne smary </h1>
+      </div>
+
+      <div className='oleje-cards-sell'>
+        {olejeProdukt.map ( (item , index) => {
+          return(
+            <div className='oleje-card-one' key={index}>
+                <img src={item.img} alt={item.index}></img>
+                <h1>{item.nazwa}</h1>
+                <p>{item.pojemnosc}</p>
+                <h3>{item.cena}</h3>
+              </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
