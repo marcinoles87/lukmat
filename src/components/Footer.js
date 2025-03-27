edf0eec3af966589b1282d0c85c52131cc4a9f02
marcinoles87 <marcinoles87@gmail.com>
2025-03-27 08:41:28 +0100

    add new function

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,8 @@ function Footer() {
         <h3>Email : lukmat@gmail.com</h3>
         <h3>Phone : 666-666-666</h3>
         <h3>Adres : Ostrów 156 Proszowice</h3>
-        <h3>Części : dzial handlowy 455-555-555 </h3>
+        <h3>Części : 455-555-555 </h3>
+        <h3>Dzial handlowy : 455-555-555 </h3>
       </div>
 
       <div>
